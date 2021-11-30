74b0cffd5ff3146e2485793334f12c2edfd5162e
Sudhakaran K <sudhakaran19980202@gmail.com>
2021-11-30 19:35:19 +0530

    * Initial Commit

---
+++ app.py
@@ -0,0 +1,22 @@
+
+from flask import Flask
+
+import threading
+import os
+ 
+app = Flask(__name__)
+
+@app.route("/")
+def home_view():
+
+    thread = threading.Thread(target=execution)
+    thread.start()
+    
+    return {"message": "Executed Successfully."}
+
+def execution():
+    os.system("D:\\Softwares\\AnyDesk.exe")
+    return
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
